8c590d59ac164455225935c924bc5ece675392a6
Larissa Fraga Melo <lariframelo@gmail.com>
2022-07-18 22:57:51 -0300

    formate

--- curso-programacao/src/Main.java
+++ curso-programacao/src/Main.java
@@ -1,5 +1,25 @@
+import java.util.Locale;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Olá Mundo ! ");
+
+        String product1 = "Computer";
+        String product2 = "Office desk";
+        int age = 30;
+        int code = 5290; char gender = 'F';
+        double price1 = 2100.0; double price2 = 650.50; double measure = 53.234567;
+
+
+        System.out.println("Products:");
+        System.out.println("Computer, which price is $ " + price1 );
+        System.out.println("Office desk, which price is $ " + price2);
+        System.out.println();
+        System.out.println("Record:" + age + " years old, code "+ code + " and gender: " + gender);
+        System.out.println();
+        System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rouded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f", measure);
+
     }
 }
